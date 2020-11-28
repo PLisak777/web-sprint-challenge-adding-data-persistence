91a17a1f2e4518a4eb5eb7fdb000b2334ed1591c
Phil Lisak <phil.lisak@gmail.com>
2020-11-28 12:35:21 -0700

    server.js and index.js setup and ready

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// start your server here
+const server = require('./server')
+
+const PORT = process.env.PORT || 5000
+
+server.use('/api/projects', require('./projects/projectsRouter'))
+server.use('/api/resources', require('./resources/resourceRouter'))
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
