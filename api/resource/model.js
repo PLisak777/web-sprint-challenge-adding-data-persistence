1cd79f33e53fce038b0731538af4319dbfeb4eee
Phil Lisak <phil.lisak@gmail.com>
2020-11-28 16:33:45 -0700

    Added resources model.js

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,33 @@
-// build your `Resource` model here
+const db = require('knex')(require('../../knexfile').development);
+
+function getResources() {
+	return db('resources');
+}
+
+function getResourceById(id) {
+	return db('resources').where({ id }).first();
+}
+
+async function addResource(resource) {
+	const [id] = await db('resources').insert(resource);
+
+	return getResourceById(id);
+}
+
+function getProjectsByResource(resourceId) {
+	return db('projects_resrouces as ref')
+		.join('projects as p', 'ref.projectId', 'p.id')
+		.where({ 'ref.resourceId': resourceId })
+		.select('p.*')
+		.then((res) => {
+			console.log(res);
+			return res;
+		});
+}
+
+module.exports = {
+	getResources,
+	addResource,
+	getResourceById,
+	getProjectsByResource,
+};
